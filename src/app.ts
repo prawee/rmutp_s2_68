a85945e4b9e969c0b2d0e4bcf542fad6aae27dfb
Prawee Wongsa <prawee@hotmail.com>
2025-08-31 15:26:08 +0700

    add more scripts with dev

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World"));
+app.get("/", (c) => c.text("Hello World Today!"));
 app.get("/profile", (c) => c.text("Profile"));
 
 export default app;
