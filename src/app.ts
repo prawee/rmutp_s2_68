01035ec944f160b1ffc0e59285e893230f2b156f
Prawee Wongsa <prawee@hotmail.com>
2025-08-31 16:22:57 +0700

    using prisma client call to db by profile

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello World Today!"));
-app.get("/profile", (c) => c.text("Profile"));
+app.get("/profile", async (c) => {
+    //get data from db
+    const profiles = await prisma.profile.findMany();
+    //response
+    return c.json({
+        message: "get data completed",
+        data: profiles
+    }, 200);
+});
 
 export default app;
