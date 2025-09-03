a20d429c7b822c680645acab17154885a590772d
Prawee Wongsa <prawee@hotmail.com>
2025-09-03 17:07:18 +0700

    change hash with 13 round

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.post("/profile", async (c) => {
     console.log('body.password(original) ', body.password);
 
     //encode password
-    const passwordHash = await bcrypt.hash(body.password, 10);
+    const passwordHash = await bcrypt.hash(body.password, 13);
     console.log('hash.password(after) ', passwordHash);
     body.password = passwordHash;
     console.log('body.password(replace) ', body);
