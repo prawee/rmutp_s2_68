cdd3be919417ce36c49a7565ad9f0993cf8c3e9d
Prawee Wongsa <prawee@hotmail.com>
2025-09-03 17:10:52 +0700

    save to db and response

--- src/app.ts
+++ src/app.ts
@@ -27,10 +27,16 @@ app.post("/profile", async (c) => {
     console.log('hash.password(after) ', passwordHash);
     body.password = passwordHash;
     console.log('body.password(replace) ', body);
+    
     //save to db
+    const result = await prisma.profile.create({
+        data: body
+    });
+
     //output response
     return c.json({
-        message: "create profile completed"
+        message: "create profile completed",
+        data: result
     });
 });
 
