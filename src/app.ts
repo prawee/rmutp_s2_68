2fc2a2116ab151eefdf3aec5c1721314640a1535
Prawee Wongsa <prawee@hotmail.com>
2025-09-17 16:32:23 +0700

    update 17092025

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
 import * as bcrypt from "bcrypt";
 import { Md5 } from "md5-typescript";
-import { encode, decode } from "./security";
+// import { encode, decode } from "./security";
+import { encode,decode } from "./service";
 
 const prisma = new PrismaClient();
 
@@ -36,10 +37,12 @@ app.post("/profile", async (c) => {
     // console.log('body.password(replace) ', body);
 
     //encode mobile
-    body.mobile = Md5.init(body.mobile);
+    // body.mobile = Md5.init(body.mobile);
+    body.mobile = encode(body.mobile);
 
     //encode cardId
-    body.cardId = Md5.init(body.cardId);
+    // body.cardId = Md5.init(body.cardId);
+    body.cardId = encode(body.cardId);
 
     //data before save
     console.log('data before save ', body);
@@ -80,6 +83,10 @@ app.get("/profile/:id", async (c) => {
         }
     });
     delete profile.password;
+    console.log('cardId', profile.cardId.length);
+    console.log('mobile', profile.mobile.length);
+    profile.cardId = decode(profile.cardId);
+    // profile.mobile =
 
     return c.json({
         message: "get data completed",
@@ -113,13 +120,13 @@ app.post("/login", async (c) => {
 app.post("/encode", async (c) => {
     return c.json({
         message: "encode completed",
-        func: encode(),
+        func: encode('abcd'),
     });
 });
 app.post("/decode", async (c) => {
     return c.json({
         message: "decode completed",
-        func: decode(),
+        func: decode('sldldldld'),
     });
 });
 
---
+++ src/service.ts
@@ -0,0 +1,26 @@
+import * as crypto from "crypto";
+
+const algorithm = "aes-256-cbc";
+// const key = crypto.randomBytes(32);
+const key = process.env.KEY_SECRET;
+// const iv = crypto.randomBytes(16);
+const iv = process.env.IV_SECRET;
+
+export const encode = (password) => {
+    console.log(`---------------------------encode > `, Buffer.from(key), Buffer.from(iv));
+    const encodeCipher = crypto.createCipheriv(algorithm, key, iv);
+    // console.log("encodeCipher ", encodeCipher);
+    const encrypted = encodeCipher.update(password, 'utf-8', 'base64');
+    const final = encrypted + encodeCipher.final('base64');
+    console.log("encrypted(final)", final, final.length);
+    return final;
+};
+export const decode = (encryptedStr: any) => {
+    console.log(`---------------------------decode > `, Buffer.from(key), Buffer.from(iv));
+    const decodeCipher = crypto.createDecipheriv(algorithm, key, iv);
+    // console.log("decodeCipher ", decodeCipher);
+    const decrypted = decodeCipher.update(encryptedStr, 'base64', 'utf-8');
+    const d_final = decrypted + decodeCipher.final('utf-8');
+    console.log("decrypted(final) ", d_final);
+    return d_final;
+};
